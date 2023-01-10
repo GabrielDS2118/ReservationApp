478425780446dc0f6270377500831777f64f36fc
Gabriel Aristizabal <gabrielhotm2020@outlook.com>
2023-01-09 19:20:51 -0500

    6.Register y Login

--- Backend/routes/auth.route.js
+++ Backend/routes/auth.route.js
@@ -1,7 +1,10 @@
 import express from 'express';
 
+import { login, register } from '../controllers/auth.controller.js';
+
 const routerAuth = express.Router();
 
-routerAuth.get('/register');
+routerAuth.post('/register', register);
+routerAuth.post('/login', login);
 
 export default routerAuth;
